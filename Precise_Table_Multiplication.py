71451486d1b33d8f3a1a7714f9741fd4291349ce
Sujoy Das <sujoydas96>
2020-11-17 07:47:06 +0530

    Precise Multiplication table Practice

--- Precise_Table_Multiplication.py
+++ Precise_Table_Multiplication.py
@@ -1,11 +1,12 @@
 import random 
 
 count = 0
+a = int(input("Enter the multiplication table for practice: "))
+
 while True:
-    a = int(random.choice([2,3,4,5,6,7,8,9,11,12,13,14,15,16]))
     b = int(random.choice([2,3,4,5,6,7,8,9]))
     count = count + 1
-    print("Question Number = "+str(count))	
+    print("Count = "+str(count))	
     print(str(a) + " X " + str(b))
     answer = int(input())
     if(answer == (a*b)):
