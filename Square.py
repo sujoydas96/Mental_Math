aba481f40190c8c42add3ff0bb0c32745acd2c67
Sujoy Das <das.4567@gmail.com>
2023-06-17 11:20:29 +0530

    errors fixed in square and cube programs

--- Square.py
+++ Square.py
@@ -2,13 +2,13 @@ import random
 
 try:
     while True:
-    for i in range(11,20):
-        print(str(i) + " X " + str(i))
-        answer = int(input())
-        if(answer == (i*i)):
-            print("SUCCESS!!")
-        else:
-            print("FAIL!! correct answer: " + str(i*i)) 
+        for i in range(21,30):
+            print(str(i) + " X " + str(i))
+            answer = int(input())
+            if(answer == (i*i)):
+                print("SUCCESS!!")
+            else:
+                print("FAIL!! correct answer: " + str(i*i)) 
 except:
     print("Irrelevent key has been pressed. Exiting Program")
     
